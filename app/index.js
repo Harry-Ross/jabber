8772686751d279f2cd2e20d067ce027cf98fb238
Harry Ross <hazross@hotmail.com>
2021-01-09 15:06:12 +1100

    basic chat output

--- app/index.js
+++ app/index.js
@@ -1,18 +1,17 @@
 const WebSocket = require('ws');
 const ws = new WebSocket('ws://irc-ws.chat.twitch.tv:80');
 
-window.$ = window.jquery = require('jquery')
+global.$ = window.$ = window.jquery = require('jquery')
 
 ws.on('open', function open() {
     console.log("connected")
     ws.send("PASS oauth:ax7ev0hvz4ui8ki8ru1m6dnoi1yfa6");
     ws.send("NICK hmvortex");
-    joinChat("hasanabi")
+    ws.send("CAP REQ :twitch.tv/tags")
 })
 
 ws.on('message', function incoming(data) {
-    console.log(data);
-    $("p").append(`<p>${data}</p>`);
+    appendChatMsg(parseIRC(data));
     if (data == "PING :tmi.twitch.tv") {
         ws.send("PONG :tmi.twitch.tv")
     }
@@ -22,7 +21,44 @@ function joinChat (channel) {
     ws.send(`JOIN #${channel}`);
 }
 
-// [user]![user]@[user].tmi.twitch.tv PRIVMSG #[channel] [message content]
+function attachChat() {
+    joinChat(document.getElementById("channelInput").value)
+}
+
+// :[user]![user]@[user].tmi.twitch.tv PRIVMSG #[channel] [message content]
 function parseIRC(messageString) {
-    messageString.search(":")
+    if (messageString.split(" ")[2] == "PRIVMSG") {
+        let messageContent = messageString.slice(messageString.search(" "), messageString.length)
+        let metadata = collectTags(messageString)
+        
+        return {
+            type: messageString.split(" ")[2],
+            user: messageContent.slice(2, messageContent.search("!")),
+            channel: messageContent.split("#")[1].slice(0, messageContent.split("#")[1].search(" ")),
+            content: messageContent.split("#")[1].slice(messageContent.split("#")[1].search(" ")+2, messageContent.split("#")[1].length-1),
+            metadata
+        }
+    } else {
+        return null;
+    }
+}
+
+function collectTags(messageString) {
+    let metadata = {}
+    messageString.split(" ")[0].split(";").map(i => {
+        metadata[i.split("=")[0]] = i.split("=")[1]
+    })
+    return metadata;
+}
+
+const ejs = require("ejs");
+
+function appendChatMsg(chatmsg) {
+    let chatbox = document.getElementById("chatbox");
+    ejs.renderFile('app/chatmsg.ejs', { chatmsg }, {}, function (err, str) {
+        if (err) console.error(err)
+        const parser = new DOMParser();
+        let chatobj = parser.parseFromString(str, 'text/html');
+        chatbox.append(chatobj.body)
+    })  
 }
